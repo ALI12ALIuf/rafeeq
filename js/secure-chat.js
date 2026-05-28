4fa1665c9b102ee4789f8116087b044504f196cb
ALI12ALIuf <iidakod57@gmail.com>
2026-05-28 15:40:11 +0300

    Update secure-chat.js

--- js/secure-chat.js
+++ js/secure-chat.js
@@ -20,7 +20,7 @@ const SecureChatSystem = {
             console.log('🔐 بدء تهيئة نظام التشفير...');
             await this.setupKeys();
             this.startReceiving();
-            // ✅ تم إزالة PresenceSystem.setOnline() (إلغاء حالة الاتصال نهائياً)
+            PresenceSystem.setOnline();
             console.log('✅ تم تهيئة نظام التشفير بنجاح');
             return true;
         } catch (error) {
