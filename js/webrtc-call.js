6532c975f42c59303f21a03e5d64a32ddc2c6c17
ALI12ALIuf <iidakod57@gmail.com>
2026-06-09 14:11:24 +0000

    Delete js/webrtc-call.js

--- js/webrtc-call.js
+++
@@ -1,8 +0,0 @@
-// ========== webrtc-call.js ==========
-const CallSystem = {
-    test: function() {
-        console.log('CallSystem يعمل');
-    }
-};
-
-console.log('✅ webrtc-call.js تم تحميله');
