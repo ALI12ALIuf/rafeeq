431ddaa2aa2400ed73cacd12c0cb4f7e255fc202
ALI12ALIuf <iidakod57@gmail.com>
2026-06-09 14:34:43 +0000

    Create webrtc-call.js

---
+++ js/webrtc-call.js
@@ -0,0 +1,11 @@
+// ========== webrtc-call.js ==========
+console.log('✅✅✅ 1. webrtc-call.js بدأ التحميل');
+
+const CallSystem = {
+    test: function() {
+        console.log('CallSystem يعمل');
+    }
+};
+
+console.log('✅✅✅ 2. webrtc-call.js انتهى التحميل بنجاح');
+console.log('typeof CallSystem:', typeof CallSystem);
