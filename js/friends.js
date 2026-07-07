ee6f97fbdf29df7a2d8c07060cfde281e8941035
ALI12ALIuf <iidakod57@gmail.com>
2026-07-08 01:22:43 +0300

    Update friends.js

--- js/friends.js
+++ js/friends.js
@@ -536,7 +536,11 @@ window.getEmojiForUser = function(userData) {
         'woman_medium': '👩🏼',
         'woman_dark': '👩🏽'
     };
-    return emojiMap[userData?.avatarType] || '🧔🏻‍♂️';
+    // دعم التوافق مع المستخدمين القدامى
+    if (!userData?.avatarType || ['male','female','boy','girl','father','mother','grandfather','grandmother'].includes(userData.avatarType)) {
+        return '🧔🏻‍♂️';
+    }
+    return emojiMap[userData.avatarType] || '🧔🏻‍♂️';
 };
 
 window.loadFriendRequestsForChat = loadFriendRequestsForChat;
